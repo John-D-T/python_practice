6c7eeaddc111df142fbbb6c809f0b15878c9cebb
John T <john.david.tringham@gmail.com>
2023-12-21 23:31:23 +0800

    Add: Day 2 part 1 solution

--- problems/advent_of_code/day_2/day_2_part_1.py
+++ problems/advent_of_code/day_2/day_2_part_1.py
@@ -12,6 +12,8 @@ Breakdown:
     Initialize variable 'valid_total'. Set to 0 initially.
 
     Loop through each game (a row)
+        set up a 0 for green, blue and red. We'll figure out the max over time
+
         Contains different rounds (split by semi-colon)
 
         Loop through each round:
@@ -24,6 +26,58 @@ Note: this would be g x r time complexity (number of games x number of rounds)
 Improvement: Could instead iterate through the round, obtain the max, then compare to the max values
 """
 
+def determine_plausible_games(max_cubes, games):
+
+    valid_total = 0
+
+    for game in games:
+        id = game.split(': ')[0].split(' ')[-1]
+        game = game.split(': ')[-1]
+        max_dict = {
+            "blue": 0,
+            "red": 0,
+            "green": 0
+        }
+        rounds = game.split('; ')
+        for round in rounds:
+            list = round.split(', ')
+            for colours in list:
+                colour = colours.split(' ')[1]
+                count = colours.split(' ')[0]
+
+                max_dict[colour] = max(int(count), max_dict[colour])
+
+        implausible = False
+
+        for colour in max_dict.keys():
+            if max_dict[colour] > max_cubes[colour]:
+                implausible = True
+
+        if not implausible:
+            valid_total += int(id)
+
+
+    return valid_total
+
+
+
+
+
+if __name__ == "__main__":
+
+    max_cubes = {
+        "red": 12,
+        "green": 13,
+        "blue": 14
+    }
+
+    with open('day_2_input.txt') as f:
+        # lines = f.readlines()
+        lines = f.read().splitlines()
+
+    valid_total = determine_plausible_games(max_cubes=max_cubes, games=lines)
+
+    print(valid_total)
 
 
 
