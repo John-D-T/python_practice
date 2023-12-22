49f1a23c661c3cb36567f3f63a668f31fc20ec4b
John T <john.david.tringham@gmail.com>
2023-12-22 17:53:00 +0800

    Add: Day 4 part 1: (wip)

--- problems/advent_of_code/day_4/day_4_part_1.py
+++ problems/advent_of_code/day_4/day_4_part_1.py
@@ -27,4 +27,46 @@ Loop through the numbers:
         add 2^n to total_points
 
 return total points
+
+Soln: 1131 (too low)
 """
+
+
+def calculate_total_points(card_matrix):
+
+    total_points = 0
+    for row in card_matrix:
+        winning_hashMap = {}
+
+        counter = 0
+        card_number, numbers = row.split(': ')[0], row.split(': ')[-1]
+        existing_numbers = numbers.split(' | ')[-1].split(' ')
+
+        winning_numbers = numbers.split('|')[0].split(' ')
+        for number in winning_numbers:
+            if number.isnumeric():
+                winning_hashMap[number] = winning_hashMap.get(number) + 1 if winning_hashMap.get(number) else 1
+
+        for number in existing_numbers:
+            if winning_hashMap.get(number):
+                winning_hashMap[number] = winning_hashMap.get(number) - 1
+                counter += 1
+
+                if winning_hashMap.get(number) <= 0:
+                    del winning_hashMap[number]
+
+        exponential_counter = 2 ** (counter - 1)
+
+        total_points += exponential_counter
+
+    return total_points
+
+
+if __name__ == '__main__':
+
+    with open("day_4_input.txt") as f:
+        file = f.read().splitlines()
+
+    total_points = calculate_total_points(card_matrix=file)
+
+    print(total_points)
