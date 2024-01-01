d82c58815ef96c614d60575817319b47b24426df
John T <john.david.tringham@gmail.com>
2024-01-01 22:33:43 +0800

    Add: Problem 104 (wip)

---
+++ problems/104.py
@@ -0,0 +1,7 @@
+# this is a DFS or BFS problem
+
+# explore each path, and derive a level from that path
+
+# then calculate the max level from all those levels - thats our maximum depth
+
+# try for a stack
